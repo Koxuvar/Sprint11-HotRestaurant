3a3668b57682a4fea4ebf5ad2b25ff2df15aabc2
MMVHome <connortsullivan1@gmail.com>
2021-04-12 19:31:36 -0500

    added server and tables and stuff

---
+++ data/tableData.js
@@ -0,0 +1,11 @@
+
+const tableArray = [
+    {
+        "customerName"  : "Dude",
+        "customerEmail" : "Dude@Dudenet.com",
+        "ID"            : "001",
+        "phoneNumber"   : "3122222222",
+    },
+];
+
+module.exports = tableArray;
--- server.js
+++ server.js
@@ -1,1 +1,17 @@
 const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+
+require('./routes/apiRoutes.js')(app);
+require('./routes/htmlRoutes.js')(app);
+
+app.listen(PORT, () =>
+{
+    console.log('Listening on port: ' + PORT);
+});
